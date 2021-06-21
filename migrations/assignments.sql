8c196711b2c8ab2e9f58bfb452df3954a5ca7604
Jacob <jacob.fitzgerald@outlook.com>
2021-06-21 22:49:43 +0000

    add 2 queries finding total project submission times

--- migrations/assignments.sql
+++ migrations/assignments.sql
@@ -1,8 +1,8 @@
 create table assignments (
   id serial primary key not null,
-  name varchar(255) not null,
-  content text not null,
-  day integer not null,
-  chapter integer not null,
-  duration integer not null
+  name varchar(255),
+  content text,
+  day integer,
+  chapter integer,
+  duration integer
 );
